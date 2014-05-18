8e78adfda967c33246bfc2f35d376fc571328ccd
Connor Doyle <connor@mesosphere.io>
2014-05-17 18:29:14 -0700

    Fixed bug in executorId check

--- RenderingCrawler.py
+++ RenderingCrawler.py
@@ -1,21 +1,5 @@
 #!/usr/bin/env python
 
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from collections import deque
 import json
 import os
@@ -126,8 +110,9 @@ class RenderingCrawler(mesos.Scheduler):
                     self.renderQueue.append(url)
                     self.processedURLs.add(url)
 
-        elif executorId == renderExecutor.executor_id.value:
+        elif executorId.value == renderExecutor.executor_id.value:
             result = results.RenderResult(o['taskId'], o['url'], o['imageUrl'])
+            print "Appending [%s] to render results" % repr((result.url, result.imageUrl))
             self.renderResults[result.url] = result.imageUrl
 
 if __name__ == "__main__":
