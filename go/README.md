ba48ca98b7ee88bb06b22e029583bf8f733b49a0
Niklas Q. Nielsen <niklas@mesosphere.io>
2014-07-18 15:12:44 -0700

    Split dot graph generation into separate file and added readme

---
+++ go/README.md
@@ -0,0 +1,13 @@
+Go Rendler Framework
+========
+
+The way too short build instructions:
+
+```bash
+export GOPATH=$PWD
+go get code.google.com/p/goprotobuf/{proto,protoc-gen-go}
+go get github.com/mesosphere/mesos-go/mesos
+go install rendler
+./bin/rendler -master <leading-master> -local
+```
+
--- go/src/rendler/rendler.go
+++ go/src/rendler/rendler.go
@@ -10,11 +10,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-  "bufio"
-  "crypto/sha256"
-  "encoding/hex"
   "os/signal"
-  "strings"
 )
 
 const TASK_CPUS = 0.1
@@ -280,57 +276,4 @@ func executorURIs() []*mesos.CommandInfo_URI {
 		pathToURI(baseURI + "results.py", false),
 		pathToURI(baseURI + "task_state.py", false),
 	}
-}
-
-func writeDOTFile(crawlResults *list.List, renderResults map[string]string) {
-  fo, err := os.Create("result.dot")
-  if err != nil { panic(err) }
-  w := bufio.NewWriter(fo)
-
-  w.WriteString("digraph {\n")
-  w.WriteString("  node [shape=box];\n")
-
-  urlsWithImages := make(map[string]string)
-
-  for k, v := range renderResults {
-    fmt.Printf("render:%s:%s\n", k, v)
-
-    hash_bytes := sha256.Sum256([]byte(k))
-    hash := hex.EncodeToString(hash_bytes[:32])
-
-    urlsWithImages[hash] = k
-
-    // TODO(nnielsen): Support remote mode.
-    // TODO(nnielsen): Strip prepended 'file:///'.
-    localFile := "file:///"
-    path := ""
-    if strings.HasPrefix(v, localFile) {
-      path = v[len(localFile):]
-    }
-
-    w.WriteString("  X" + hash + " [label=\"\" image=\"" + path + "\"];\n")
-  }
-
-  for e := crawlResults.Front(); e != nil; e = e.Next() {
-    from := e.Value.(Edge).From
-    from_hash_bytes := sha256.Sum256([]byte(from))
-    from_hash := hex.EncodeToString(from_hash_bytes[:32])
-
-    to := e.Value.(Edge).To
-    to_hash_bytes := sha256.Sum256([]byte(to))
-    to_hash := hex.EncodeToString(to_hash_bytes[:32])
-
-    if _, ok := urlsWithImages[to_hash]; !ok {
-      continue
-    }
-    if _, ok := urlsWithImages[from_hash]; !ok {
-      continue
-    }
-    w.WriteString("  X" + from_hash + " -> X" + to_hash + ";\n")
-  }
-
-  w.WriteString("}\n")
-
-  w.Flush()
-  fo.Close()
 }
